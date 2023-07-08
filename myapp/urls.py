a3a9d2d8ad53f48246cba97f7da189f9d7691d0a
jorgechdea <jorgechdea@gmail.com>
2023-07-08 17:30:28 -0600

    crud routes ready

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import list_license, create_license, delete_license
+from .views import list_license, create_license, delete_license, update_license
 urlpatterns = [
     path('', list_license),
     path('new_license/', create_license, name="create_license"),
-    path('delete_license/<int:license_id>/', delete_license, name="delete_license")
+    path('delete_license/<int:license_id>/', delete_license, name="delete_license"),
+    # path('update_license/<int:license_id>/', update_license, name="update_license")
+    path('license/<int:license_id>/update/', update_license, name='update_license'),
+
 
 ]
