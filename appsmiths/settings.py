b97f9d808e54a79837dcf76fcaae480a58eff36a
jorgechdea <jorgechdea@gmail.com>
2023-07-08 21:58:16 -0600

    fixing db config

--- appsmiths/settings.py
+++ appsmiths/settings.py
@@ -82,7 +82,10 @@ WSGI_APPLICATION = 'appsmiths.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600 
+    )
     #  "default": {
     #     "ENGINE": "django.db.backends.postgresql",
     #     "NAME": "mydatabase",
